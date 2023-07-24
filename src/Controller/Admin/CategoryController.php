8baac35a27f005a3058787e1b478a80271a4ca58
Bahaaeddine <bahaaeddine.ghezz@gmail.com>
2023-07-24 13:19:18 +0200

    first commit

---
+++ src/Controller/Admin/CategoryController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/admin/category')]
+class CategoryController extends AbstractController
+{
+    #[Route('/', name: 'app_category')]
+    public function index(): Response
+    {
+        return $this->render('category/index.html.twig', [
+            'controller_name' => 'CategoryController',
+        ]);
+    }
+}
